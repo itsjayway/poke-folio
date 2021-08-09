4d5382c414b59b67c6cfd6a4203e2ea3c584e1aa
jibranabsarulislam <jga26@njit.edu>
2021-08-09 01:37:43 -0400

    add soft skills, fix spacing, remove blinking

--- pokemonpage/email.php
+++ pokemonpage/email.php
@@ -1,9 +1,15 @@
 <?php
 	$name = $_GET["sender_name"];
-  $company = $_GET["sender_company"];
+  if (empty($_GET["sender_company"])) {
+    $company = "N/A";
+  }
+  else {
+    $company = $_GET["sender_company"];
+  }
 	$message = $_GET["sender_message"];
   // constants
-  $subject = "Message from $name, $company"
+
+  $subject = "Message from $name, $company";
 	$to = "jga26@njit.edu";
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
