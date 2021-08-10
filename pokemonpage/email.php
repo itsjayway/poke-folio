63021faf4184d0fc66e24e53e4eae3152141e3fb
jibranabsarulislam <jga26@njit.edu>
2021-08-09 22:59:07 -0400

    proud

--- pokemonpage/email.php
+++ pokemonpage/email.php
@@ -6,13 +6,18 @@
   else {
     $company = $_GET["sender_company"];
   }
+	$email = $_GET["sender_email"];
 	$message = $_GET["sender_message"];
   // constants
 
-  $subject = "Message from $name, $company";
+  $subject = "Message from $name, $company, $email";
 	$to = "jga26@njit.edu";
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
 	mail($to, $subject, $message, $headers);
+
+	echo "Message sent! Redirecting you home...";
+	header("refresh: 3, url=../index.html");
 
 ?>
